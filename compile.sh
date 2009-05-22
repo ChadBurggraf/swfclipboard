40375587d40434815bdd6ce37ed25b3bf7d42a03
Chad Burggraf <chadb@chadb-nac.local>
2009-05-22 14:55:35 -0700

    Re-factored code to improve encapsulation.
    Updated README with full instance function list.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-java -jar lib/yuicompressor-2.4.1.jar --nomunge src/SWFClipboard.js -o build/swfclipboard.js
-java -jar lib/yuicompressor-2.4.1.jar --nomunge src/SWFClipboard.js -o demo/swfclipboard.js
+if [ "$1" == "--nocompress" ]; then
+	cat src/SWFClipboard2.js > build/swfclipboard.js
+else
+	java -jar lib/yuicompressor-2.4.1.jar src/SWFClipboard2.js -o build/swfclipboard.js
+fi
+
+cp -f build/swfclipboard.js demo/swfclipboard.js
 
 "mxmlc" \
 	-benchmark="false" \
@@ -12,11 +17,4 @@ java -jar lib/yuicompressor-2.4.1.jar --nomunge src/SWFClipboard.js -o demo/swfc
 	-file-specs="src/SWFClipboard.as" \
 	-o="build/swfclipboard.swf"
 	
-"mxmlc" \
-	-benchmark="false" \
-	-debug="false" \
-	-incremental="true" \
-	-optimize="true" \
-	-warnings="true" \
-	-file-specs="src/SWFClipboard.as" \
-	-o="demo/swfclipboard.swf"
+cp -f build/swfclipboard.swf demo/swfclipboard.swf
--- src/SWFClipboard.js
+++ src/SWFClipboard.js
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2009 Chad Burggraf
+ * Version: 1.1, 2009/05/22
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -23,340 +24,347 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-//
-// Creates a JavaScript/Flash bridge for copying text to a user's clipboard.
-// Version: 1.0, 2008/12/01
-//
-var SWFClipboard = function(target, copyText, options) {
-	this.target = target;
-	this.copyText = copyText || "";
-	this.eventQueue = [];
-	this.isFlashReady = false;
-	this.movieName = SWFClipboard.getNextMovieName();
+(function () {
+	var SWFClipboard,
+		count = 0,
+		instances = {};
+		
+	/*
+	 * Utility functions.
+	 */
 
-	this.options = {
-		css: "swfclipboard",
-		flashUrl: "swfclipboard.swf",
-		navigate: true,
-		navigateUrl: null,
-		navigateTarget: "_blank",
-		onFlashReady: null,
-		onTextCopied: null,
-		waitForLoad: false
-	};
-    
-	for(var prop in options) {
-		if (options.hasOwnProperty(prop)) {
-			this.options[prop] = options[prop];
+	// Adds an SWFClipboard instance to the instance collection.
+	function addInstance(movieName, instance) {
+		if (!instances[movieName]) {
+			instances[movieName] = instance;
+			count = count + 1;
 		}
 	}
-    
-	SWFClipboard.add(this.movieName, this);
-    
-	if (this.options.waitForLoad) {
-		var that = this, f = function() { that.setup(); };
 
-		if (window.addEventListener) {
-			window.addEventListener("load", f);
-		} else {
-			window.attachEvent("onload", f);
-		}
-	} else {
-		this.setup();
+	// Gets the cumulative offset of an element.
+	function cumulativeOffset(element) {
+		var t = 0,
+			l = 0;
+
+		do {
+			t = t + (element.offsetTop || 0);
+			l = l + (element.offsetLeft || 0);
+			element = element.offsetParent;
+		} while (element);
+
+		return {top: t, left: l};
 	}
-};
 
-//
-// Instance function definitions.
-//
+	// Extends a destination object by the source object.
+	function extend(dest, source) {
+		source = source || {};
+		dest = dest || {};
 
-SWFClipboard.prototype = {	
-	// Calls a flash function.
-    callFlash:function(callee, args) {
-        if (this.element) {
-            if (!this.isFlashReady) {
-				var that = this;
+		var prop;
+
+		for (prop in source) {
+			if (source.hasOwnProperty(prop) && typeof source[prop] !== "undefined") {
+				dest[prop] = source[prop];
+			}
+		}
+
+		return dest;
+	}
+
+	// Gets the name to use for the next SWFClipboard instance created.
+	function getNextMovieName() {
+		return "SWFClipboard_" + count;
+	}
+
+	// Removes an SWFClipboard instance from the instance collection.
+	function removeInstance(movieName) {
+		if (instances[movieName]) {
+			instances[movieName] = null;
+			delete instances[movieName];
+			count = count - 1;
+		}
+	}
+	
+	//
+	// Constructor. Creates a JavaScript/Flash bridge for copying text to a user's clipboard.
+	//
+	SWFClipboard = function (target, copyText, options) {
+		target = target || "";
+		copyText = copyText || "";
+		options = extend({
+			css: "swfclipboard",
+			flashUrl: "swfclipboard.swf",
+			navigate: true,
+			navigateUrl: null,
+			navigateTarget: "_blank",
+			onFlashReady: null,
+			onTextCopied: null,
+			waitForLoad: false
+		}, options);
+		
+		var obj = {},
+			element,
+			movieElement,
+			eventQueue = [],
+			isFlashReady = false,
+			callTryCount = 0,
+			movieName = getNextMovieName();
+		
+		/*
+		 * Private members.
+		 */
+		
+		// Calls a Flash function.
+		function callFlash(callee, args) {
+			var js,
+				i,
+				n;
 				
-				setTimeout(function() {
-					that.callFlash(callee, args);
-				}, 10);
-            } else {
-                args = args || [];
-                
-                // We'll get fits here and there if we just use
-                // apply() or call(), so unfortunately an eval()
-                // is the best we've got.
-                if (typeof this.movie()[callee] === "function") {
-					var js = 'this.movie()[callee](';
-                
-                    for(var i = 0, n = args.length; i < n; i++) {
-                        if (i > 0) {
-                            js += ",";
-                        }
-                        
-                        js += "args[" + i + "]";
-                    }
-                    
-                    return eval(js + ")");
-                } else {
-                    throw "There is no Flash \"" + callee + "\" function defined.";
-                }
-            }
-        }
-    },
-    
-    // Destroys this instance and all of its DOM references.
-    destroy:function() {
-        SWFClipboard.remove(this.movieName);
-        
-        if (this.movieElement) {
-            this.movieElement = null;
-        }
-        
-        if (this.element) {
-            this.element.parentNode.removeChild(this.element);
-            this.element = null;
-        }
-        
-        this.isFlashReady = false;
-    },
-
-	// Executes the next event in the event queue.
-    executeEvent:function() {
-        if (this.eventQueue.length > 0) {
-            var f = this.eventQueue.shift();
-
-            if (typeof f === "function") {
-                f();
-            }
-        }
-    },
-	
-	// Gets a reference to this intance's movie element.
-    movie:function() {
-        if (this.element) {
-            if (!this.movieElement) {
-                this.movieElement = document.getElementById(this.movieName);
-
-                if (!this.movieElement) {
-                    throw "Could not get a reference to movie \"" + this.movieName + "\".";
-                }
-            }
-        }
-
-        return this.movieElement;
-    },
-
-	// Queues an event. The handler must be an event handler 
-    // that is an instance member of SWFClipboard.
-    queueEvent:function(handler, args) {
-        args = args || [];
-
-        if (typeof this[handler] === "function") {
-			var that = this;
-			
-            this.eventQueue.push(function() {
-				that[handler].apply(that, args);
-            });
-
-            setTimeout(function() {
-                that.executeEvent();
-            }, 0);
-        } else {
-            throw "There is no \"" + handler + "\" event handler defined.";
-        }
-    },
-
-	// Refreshes the movie element to match the size
-	// and position of the target element.
-	refreshSize:function() {
-		// Gets the cumulative offset of an element.
-		function co(element) {
-			var t = 0, l = 0;
-			
-			do {
-				t += element.offsetTop || 0;
-				l += element.offsetLeft || 0;
-				element = element.offsetParent;
-			} while(element);
-			
-			return {top:t, left:l};
-		}
-		
-	    if (this.element) {
-		    if (this.isFlashReady && this.target) {
-			    var size = {width:this.target.offsetWidth, height:this.target.offsetHeight};
-			    var pSize = {width:this.target.parentNode.offsetWidth, height:this.target.parentNode.offsetHeight};
-			    var location = co(this.target);
-			
-			    if (size.width > pSize.width) {
-			        size.width = pSize.width;
-			    }
-			    
-			    if (size.height > pSize.height) {
-			        size.height = pSize.height;
-			    }
-    			
-				this.element.style.width = size.width + "px";
-				this.element.style.height = size.height + "px";
-				this.element.style.left = location.left + "px";
-				this.element.style.top = location.top + "px";
-
-			    this.callFlash("setButtonSize", [size.width, size.height]);
-		    } else {
-				this.element.style.width = "1px";
-				this.element.style.height = "1px";
-				this.element.style.top = "0px";
-				this.element.style.left = "0px";
-		    }
-		}
-	},
-	
-	// Sets the text that will be copied 
-	// when the clipboard is activated.
-	setCopyText:function(copyText) {
-		this.copyText = copyText || "";
-		this.callFlash("setCopyText", [this.copyText]);
-	},
-	
-	// Sets the navigate behavior to use
-	// when the clipboard is activated.
-	setNavigate:function(navigateUrl, navigateTarget, navigate) {
-		this.options.navigateUrl = navigateUrl || "";
-		this.options.navigateTarget = navigateTarget || "";
-		this.options.navigate = navigate === true;
-
-	    this.callFlash("setNavigate", [
-	        this.options.navigateUrl, 
-	        this.options.navigateTarget, 
-	        this.options.navigate
-	    ]);
-	},
-	
-	// Sets the element this instance is targeting.
-	setTarget:function(target, prevent) {
-        this.target = target ?
-			(typeof target === "string" ? document.getElementById(target) : target) :
-			null;
-        
-        if (this.target) {
-            if (!prevent && this.options.navigate) {
-                
-				if (this.target.tagName.toLowerCase() === "a") {
-					this.options.navigateUrl = !this.target.href.match(/^javascript/i) ? this.target.href || "" : null;
-	                this.options.navigateTarget = this.target.target || "_blank";
+			if (element) {
+				if (!isFlashReady) {
+					if (callTryCount < 10) {
+						callTryCount = callTryCount + 1;
+						
+						setTimeout(function () {
+							callFlash(callee, args);
+						}, 10);
+					} else {
+						throw "Tried to call Flash 10 times, but the movie is still not ready. Aborting call.";
+					}
 				} else {
-					this.options.navigateUrl = null;
-				}
+					args = args || [];
+					callTryCount = 0;
+					
+					// We're going to get Flash fits if we use apply() or call(),
+					// so eval() is the best we've got.
+					if (typeof obj.movie()[callee] === "function") {
+						js = 'obj.movie()[callee](';
+						
+						for (i = 0, n = args.length; i < n; i = i + 1) {
+							if (i > 0) {
+								js = js + ",";
+							}
+							
+							js = js + "args[" + i + "]";
+						}
 
-                this.callFlash("setNavigate", [
-	                this.options.navigateUrl, 
-	                this.options.navigateTarget, 
-	                this.options.navigate === true
-	            ]);
-            }
-        }
-        
-        this.refreshSize();
-	},
-	
-	// Sets up the DOM.
-	setup:function() {
-		if (this.target && typeof this.target === "string") {
-			this.target = document.getElementById(this.target);
+						return eval(js + ")");
+					}
+				}
+			}
 		}
 		
-		this.options.navigate = this.options.navigate === true;
+		// Executes the next event in the event queue.
+		function executeEvent() {
+			if (eventQueue.length > 0) {
+				var f = eventQueue.shift();
+				
+				if (typeof f === "function") {
+					f();
+				}
+			}
+		}
 		
-		this.element = document.createElement("div");
-		document.body.appendChild(this.element);
+		// Queues an event.
+		function queueEvent(handler, args) {
+			args = args || [];
+			
+			if (typeof handler === "function") {
+				eventQueue.push(function () {
+					handler.apply(null, args);
+				});
+				
+				setTimeout(function () {
+					executeEvent();
+				}, 0);
+			}
+		}
 		
-		this.element.className = this.options.css;
-		this.element.style.position = "absolute";
-		this.element.style.display = "block";
-		this.element.style.width = "1px";
-		this.element.style.height = "1px";
-		this.element.style.left = "0px";
-		this.element.style.top = "0px";
+		// Refreshes the movie element to match the size and position of the target element.
+		function refreshSize() {
+			if (element) {
+				if (isFlashReady && target) {
+					var size = {width: target.offsetWidth, height: target.offsetHeight},
+						pSize = {width: target.parentNode.offsetWidth, height: target.parentNode.offsetHeight},
+						location = cumulativeOffset(target);
+						
+					if (size.width > pSize.width) {
+						size.width = pSize.width;
+					}
+					
+					if (size.height > pSize.height) {
+						size.height = pSize.height;
+					}
+					
+					element.style.width = size.width + "px";
+					element.style.height = size.height + "px";
+					element.style.left = location.left + "px";
+					element.style.top = location.top + "px";
+					
+					callFlash("setButtonSize", [size.width, size.height]);
+				} else {
+					element.style.width = "1px";
+					element.style.height = "1px";
+					element.style.top = "0px";
+					element.style.left = "0px";
+				}
+			}
+		}
+		
+		// Sets up the DOM.
+		function setup() {
+			if (!element) {
+				if (typeof target === "string") {
+					target = document.getElementById(target);
+				}
+				
+				element = document.createElement("div");
+				document.body.appendChild(element);
+				
+				element.className = options.css;
+				element.style.position = "absolute";
+				element.style.display = "block";
+				element.style.width = "1px";
+				element.style.height = "1px";
+				element.style.left = "0px";
+				element.style.top = "0px";
+				
+				element.innerHTML = ['<object id="', movieName, '" type="application/x-shockwave-flash" data ="', options.flashUrl, '" width="100%" height="100%">',
+					'<param name="wmode" value="transparent"/>',
+					'<param name="movie" value="', options.flashUrl, '"/>',
+					'<param name="quality" value="high"/>',
+					'<param name="menu" value="false"/>',
+					'<param name="allowscriptaccess" value="always"/>',
+					'<param name="flashvars" value="',
+						"movieName=", encodeURIComponent(movieName),
+						"&amp;copyText=", encodeURIComponent(copyText || ""),
+						"&amp;navigateUrl=", encodeURIComponent(options.navigateUrl || ""),
+						"&amp;navigateTarget=", encodeURIComponent(options.navigateTarget || "_blank"),
+						"&amp;navigate=", (!!(options.navigate)).toString(), '"/>',
+					'</object>'].join("");
+			}
+		}
+		
+		/*
+		 * Event handlers.
+		 */
+		
+		function onFlashReady() {
+			isFlashReady = true;
+			obj.setTarget(target);
+			
+			if (typeof options.onFlashReady === "function") {
+				options.onFlashReady(obj);
+			}
+		}
+		
+		function onTextCopied() {
+			if (typeof options.onTextCopied === "function") {
+				options.onTextCopied(obj);
+			}
+		}
+		
+		/*
+		 * Public members.
+		 */
+		
+		extend(obj, {
+			// Destroys this instance and all of its DOM references.
+			destroy: function () {
+				removeInstance(movieName);
+				
+				if (movieElement) {
+					movieElement = null;
+				}
+				
+				if (element) {
+					element.parentNode.removeChild(element);
+					element = null;
+				}
+				
+				isFlashReady = false;
+			},
+			
+			// Called by Flash when the movie is ready.
+			flashReady: function () {
+				if (!window[movieName]) {
+					window[movieName] = this.movie();
+				}
+				
+				queueEvent(onFlashReady);
+			},
+			
+			// Gets a reference to this instance's movie element.
+			movie: function () {
+				if (element) {
+					if (!movieElement) {
+						movieElement = document.getElementById(movieName);
+						
+						if (!movieElement) {
+							throw "Could not get a reference to movie \"" + movieName + "\".";
+						}
+					}
+				}
+				
+				return movieElement;
+			},
+			
+			// Sets the text that will be copied with the clipboard is activated.
+			setCopyText: function (newCopyText) {
+				copyText = newCopyText || "";
+				callFlash("setCopyText", [copyText]);
+			},
+			
+			// Sets the navigate behavior to use when the clipboard is activated.
+			setNavigate: function (newNavigateUrl, newNavigateTarget, newNavigate) {
+				options.navigateUrl = newNavigateUrl || "";
+				options.navigateTarget = newNavigateTarget || "";
+				options.navigate = !!(newNavigate);
+				
+				callFlash("setNavigate", [options.navigateUrl, options.navigateTarget, options.navigate]);
+			},
+			
+			// Sets the element this instance is targeting.
+			setTarget: function (newTarget, prevent) {
+				target = newTarget ? (typeof newTarget === "string" ? document.getElementById(newTarget) : newTarget) : null;
+				
+				if (target) {
+					if (!prevent && !options.navigateUrl) {
+						if (options.navigate) {
+							options.navigateUrl = !target.href.match(/^javascript/i) ? target.href : null;
+							options.navigateTarget = target.target || "_blank";
+						} else {
+							options.navigateUrl = null;
+						}
+					}
 
-		this.element.innerHTML = ['<object id="', this.movieName, '" type="application/x-shockwave-flash" data="', this.options.flashUrl, '" width="100%" height="100%">',
-		    '<param name="wmode" value="transparent" />',
-		    '<param name="movie" value="', this.options.flashUrl, '" />',
-		    '<param name="quality" value="high" />',
-		    '<param name="menu" value="false" />',
-		    '<param name="allowscriptaccess" value="always" />',
-		    '<param name="flashvars" value="',
-		        "movieName=", encodeURIComponent(this.movieName),
-		        "&amp;copyText=", encodeURIComponent(this.copyText || ""),
-		        "&amp;navigateUrl=", encodeURIComponent(this.options.navigateUrl || ""),
-		        "&amp;navigateTarget=", encodeURIComponent(this.options.navigateTarget || "_blank"),
-		        "&amp;navigate=", this.options.navigate.toString(), '" />',
-		    '</object>'].join("");
-	},
+					callFlash("setNavigate", [options.navigateUrl, options.navigateTarget, !!(options.navigate)]);
+				}
+				
+				refreshSize();
+			},
+			
+			// Called by Flash when text has been copied.
+			textCopied: function () {
+				queueEvent(onTextCopied);
+			}
+		});
+
+		if (options.waitForLoad) {
+			if (window.addEventListener) {
+				window.addEventListener("load", setup);
+			} else {
+				window.attachEvent("onload", setup);
+			}
+		} else {
+			setup();
+		}
+		
+		addInstance(movieName, obj);
+		return obj;
+	};
 	
-	//
-    // Flash callbacks. These are called directly by Flash.
-    //
-
-    flashReady:function() {
-        if (!window[this.movieName]) {
-            window[this.movieName] = this.movie();
-        }
-
-        this.queueEvent("onFlashReady");
-    },
-
-    textCopied:function() {
-        this.queueEvent("onTextCopied");
-    },
-
-	//
-    // Flash callback event handlers. These are called after
-    // their corresponding callbacks have been called and
-    // a bug-fix timeout has been executed.
-    //
-
-    onFlashReady:function() {
-        this.isFlashReady = true;
-        this.setTarget(this.target);
-
-        if (typeof this.options.onFlashReady === "function") {
-            this.options.onFlashReady(this);
-        }
-    },
-
-    onTextCopied:function() {
-        if (typeof this.options.onTextCopied === "function") {
-            this.options.onTextCopied(this);
-        }
-    }
-};
-
-//
-// Static functions and properties.
-//
-
-SWFClipboard.count = 0;
-SWFClipboard.instances = {};
-
-// Adds an instance to the instance collection.
-SWFClipboard.add = function(movieName, instance) {
-    if (!SWFClipboard.instances[movieName]) {
-		SWFClipboard.instances[movieName] = instance;
-        SWFClipboard.count++;
-    }
-};
-
-// Gets the name to use for the next SWFClipboard instance.
-SWFClipboard.getNextMovieName = function() {
-	return "SWFClipboard_" + SWFClipboard.count;
-};
-
-// Removes an instance from the instance collection.
-SWFClipboard.remove = function(movieName) {
-	if (SWFClipboard.instances[movieName]) {
-		SWFClipboard.instances[movieName] = null;
-		delete SWFClipboard.instances[movieName];
-		--SWFClipboard.count;
-	}
-};
+	// Tell the world about ourselves.
+	window.SWFClipboard = SWFClipboard;
+	window.SWFClipboard.instances = instances;
+}());
